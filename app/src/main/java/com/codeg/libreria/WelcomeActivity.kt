d9d933ce3cab214e3b81902d8065ae83b5efe1d3
LakshanGunarathna <126014037+LakshanGunarathna@users.noreply.github.com>
2024-02-26 01:02:46 +0530

    change the welcome page

--- app/src/main/java/com/codeg/libreria/WelcomeActivity.kt
+++ app/src/main/java/com/codeg/libreria/WelcomeActivity.kt
@@ -1,19 +1,42 @@
 package com.codeg.libreria
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        btnGetStart.setOnClickListener{
-            val gotoNextScreen = Intent(applicationContext,LoginActivity::class.java)
-            startActivity(gotoNextScreen)
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
 
+        if (isFirstTime()) {
+            // Show welcome activity
+            btnGetStart.setOnClickListener {
+                val editor = sharedPreferences.edit()
+                editor.putBoolean("isFirstTime", false)
+                editor.apply()
+
+                val gotoNextScreen = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(gotoNextScreen)
+                finish()
+            }
+        } else {
+            // App has already been opened before, go directly to login activity
+            val gotoNextScreen = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(gotoNextScreen)
+            finish()
         }
+    }
+
+    private fun isFirstTime(): Boolean {
+        return sharedPreferences.getBoolean("isFirstTime", true)
     }
 }

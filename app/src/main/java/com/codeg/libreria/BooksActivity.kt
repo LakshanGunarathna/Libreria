22e0df25deb246a848b40c0eea568f230aab3803
LakshanGunarathna <126014037+LakshanGunarathna@users.noreply.github.com>
2024-04-08 23:53:49 +0530

    nav bar edited

--- app/src/main/java/com/codeg/libreria/BooksActivity.kt
+++ app/src/main/java/com/codeg/libreria/BooksActivity.kt
@@ -1,7 +1,10 @@
 package com.codeg.libreria
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/codeg/libreria/HomeActivity.kt
+++ app/src/main/java/com/codeg/libreria/HomeActivity.kt
@@ -1,24 +1,51 @@
 package com.codeg.libreria
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        btnAddBook.setOnClickListener{
-            val gotoNextScreen = Intent(applicationContext, AddBookActivity::class.java)
-            startActivity(gotoNextScreen)
-        }
-
-        btnAddUser.setOnClickListener{
-            val gotoNextScreen = Intent(applicationContext, AddUserActivity::class.java)
-            startActivity(gotoNextScreen)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_home -> {
+                    val gotoNextScreen = Intent(applicationContext, HomeActivity::class.java)
+                    startActivity(gotoNextScreen)
+                    true
+                }
+                R.id.navigation_book -> {
+                    val gotoNextScreen = Intent(applicationContext, AddBookActivity::class.java)
+                    startActivity(gotoNextScreen)
+                    true
+                }
+                R.id.navigation_user -> {
+                    val gotoNextScreen = Intent(applicationContext, AddUserActivity::class.java)
+                    startActivity(gotoNextScreen)
+                    true
+                }
+                R.id.navigation_lending -> {
+                    val gotoNextScreen = Intent(applicationContext, AddUserActivity::class.java)
+                    startActivity(gotoNextScreen)
+                    true
+                }
+                R.id.navigation_admin -> {
+                    val gotoNextScreen = Intent(applicationContext, AdminActivity::class.java)
+                    startActivity(gotoNextScreen)
+                    true
+                }
+                else -> false
+            }
         }
     }
 
 }
+
